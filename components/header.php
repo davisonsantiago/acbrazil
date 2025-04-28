f2f780e668cf4a1a9fc5f0166a44f865be81fbdc
Davison Santiago <davisonsantiago@gmail.com>
2025-04-28 12:23:00 -0300

    Add files via upload

---
+++ components/header.php
@@ -0,0 +1,13 @@
+<div id="header">
+	<div id="topo">
+		<div id="logo"></div>
+		<div id="menu">
+			<ul>
+				<li><a href="/index.html">Ínicio</a></li>
+				<li><a href="/pages/about.html">AC Brazil</a></li>
+				<li><a href="/pages/services.html"></a></li>
+				<li><a href="/pages/contact.html"></a></li>
+			</ul>
+		</div>
+	</div>
+</div>
---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>AC Brazil – Consultoria Empresarial</title>
+	<script src="/js/main.js"></script>
+</head>
+<body>
+	<?php 
+		$header = include_once("./components/header.php");
+		echo $header;
+	?>
+</body>
+</html>
